ac22c385c576ad328d1d27020aad64fb78a8b052
Dan Quill <dquill5@gmail.com>
2016-12-12 18:40:21 +0000

    Have added a primitive rotation by matrix - I haven't fully checked it yet

--- cube-V2.py
+++ cube-V2.py
@@ -14,9 +14,9 @@ class Cube:
         self.cubie_colour = [0,0,0] # this will be the colour vector at each cubie point, start it off empty (as zeros)
         self.line = [self.cubie_colour,self.cubie_colour,self.cubie_colour]
         self.face = [self.line,self.line,self.line] 
-        self.cube = [self.face,self.face,self.face] # build up the cube
+        self.empty_cube = [self.face,self.face,self.face] # build up the cube
 
-        self.cube = np.array(self.cube) # np array is easier to navigate
+        self.cube = np.array(self.empty_cube) # np array is easier to navigate
 
 # next we need to set the colour vectors
         for x in range(0,3):
@@ -46,7 +46,106 @@ class Cube:
 
 # TODO - write some kind of method to print the cube in a 
 
+# Next, we need to implement rotations of the cube, where we don't
+# perform any twist, just a rotation in space
 
+    def rotate_z(self):
+
+        tmp_cube = np.array(self.empty_cube)
+        for i in range(0,3):
+            for j in range(0,3):
+                for k in range(0,3):
+
+                    x = i - 1 # centre the cube at 0,0,0
+                    y = j - 1 # so we can rotate about z axis
+                    z = k - 1
+
+                    z_matrix = [ [0,1,0], [-1,0,0], [0,0,1] ]
+
+                    [x1,y1,z1] = np.dot(z_matrix,[x,y,z])
+
+                    i1 = x1 + 1
+                    j1 = y1 + 1
+                    k1 = z1 + 1
+
+                    # tmp_cube[i1,j1,k1] = self.cube[i,j,k]
+                    
+                    tmp = self.cube[i,j,k]
+                    tmp_cube[i1,j1,k1] = [tmp[1],-tmp[0],tmp[2]]
+                    
+
+        self.cube = tmp_cube
+                    # print([x,y,z])
+                    # print([x1,y1,z1])
+                    # print()
+
+                    # print([i,j,k])
+                    # print([i1,j1,k1])
+                    # print()
+
+
+    # def rotate_cube(self,clockwise,axis):
+
+    #     position_before=[] # we will populate these lists with the
+    #     position_after=[]  # each position before and after the rotation
+
+    #     for i in range(0,3):
+    #         for j in range(0,3):
+    #             for k in range(0,3):
+
+    #                 x = i - 1 # centre the cube at 0,0,0
+    #                 y = j - 1
+    #                 z = k - 1
+
+    #                 # position_before.append([x,y,z])
+
+    #                 x_clockwise = [ [1,0,0], [0,0,1], [0,-1,0] ]
+    #                 x_anticlockwise = [ [1,0,0], [0,0,-1], [0,1,0] ]
+    #                 y_clockwise = [ [0,0,-1], [0,1,0], [1,0,0] ]
+    #                 y_anticlockwise = [ [0,0,1], [0,1,0], [-1,0,0] ]
+    #                 z_clockwise = [ [0,1,0], [-1,0,0], [0,0,1] ]
+    #                 z_anticlockwise = [ [0,-1,0], [1,0,0], [0,0,1] ]
+
+    #                 rotation_matrices =[x_clockwise,x_anticlockwise,y_clockwise,y_anticlockwise,z_clockwise,z_anticlockwise]
+
+    #                 if axis == "X":
+    #                     if clockwise == 1:
+    #                         matrix = rotation_matrices[0]
+    #                     else:
+    #                         matrix = rotation_matrices[1]
+    #                 elif axis == "Y":
+    #                     if clockwise == 1:
+    #                         matrix = rotation_matrices[2]
+    #                     else:
+    #                         matrix = rotation_matrices[3]
+    #                 elif axis == "Z":
+    #                     if clockwise == 1:
+    #                         matrix = rotation_matrices[4]
+    #                     else:
+    #                         matrix = rotation_matrices[5]
+    #                 else:
+    #                     print("can only rotate about X,Y, or Z axes, invalid input")
+
+
+    #                 position_after = np.dot(matrix,[x,y,z])
+    #                 i1 = position_after[0] + 1
+    #                 j1 = position_after[1] + 1
+    #                 k1 = position_after[2] + 1
+
+    #                 self.cube[i,j,k] = self.cube[i1,j1,k1] # this is the rotation
+
+
+
+        #             position_after.append(list(np.dot(matrix,[x,y,z])+[1,1,1]))
+
+        # # print(self.cube[position_before[1]])
+        # # print(self.cube[position_before[0]])
+        # print(self.cube position_after[0])
 
 c = Cube()
-print(c.cube[1,1,0])
+print(c.cube[2,0,0])
+print("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~")
+c.rotate_z()
+# print()
+print(c.cube[2,0,0])
+# print(c.cube[0,0])
