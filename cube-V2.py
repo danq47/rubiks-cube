d7ca652a1f169d2f2d3d0be53f9efacbfeaf2d10
Dan Quill <dquill5@gmail.com>
2016-12-12 18:56:42 +0000

    Added a full rotation method which is general, and can rotate about any axis

--- cube-V2.py
+++ cube-V2.py
@@ -49,9 +49,13 @@ class Cube:
 # Next, we need to implement rotations of the cube, where we don't
 # perform any twist, just a rotation in space
 
-    def rotate_z(self):
+    def rotate_cube(self,clockwise,axis):
+
+        if clockwise != 0 and clockwise != 1:
+            raise("first argument must be 1 or 0 (clockwise or anticlockwise rotation)")
 
         tmp_cube = np.array(self.empty_cube)
+
         for i in range(0,3):
             for j in range(0,3):
                 for k in range(0,3):
@@ -60,92 +64,51 @@ class Cube:
                     y = j - 1 # so we can rotate about z axis
                     z = k - 1
 
-                    z_matrix = [ [0,1,0], [-1,0,0], [0,0,1] ]
+                    x_clockwise = [ [1,0,0], [0,0,1], [0,-1,0] ]
+                    x_anticlockwise = [ [1,0,0], [0,0,-1], [0,1,0] ]
+                    y_clockwise = [ [0,0,-1], [0,1,0], [1,0,0] ]
+                    y_anticlockwise = [ [0,0,1], [0,1,0], [-1,0,0] ]
+                    z_clockwise = [ [0,1,0], [-1,0,0], [0,0,1] ]
+                    z_anticlockwise = [ [0,-1,0], [1,0,0], [0,0,1] ]
 
-                    [x1,y1,z1] = np.dot(z_matrix,[x,y,z])
+                    rotation_matrices = [x_clockwise,x_anticlockwise,y_clockwise,y_anticlockwise,z_clockwise,z_anticlockwise]
 
-                    i1 = x1 + 1
-                    j1 = y1 + 1
+                    if axis == "X":
+                        if clockwise == 1:
+                            matrix = rotation_matrices[0]
+                        else:
+                            matrix = rotation_matrices[1]
+                    elif axis == "Y":
+                        if clockwise == 1:
+                            matrix = rotation_matrices[2]
+                        else:
+                            matrix = rotation_matrices[3]
+                    elif axis == "Z":
+                        if clockwise == 1:
+                            matrix = rotation_matrices[4]
+                        else:
+                            matrix = rotation_matrices[5]
+                    else:
+                        raise("can only rotate about X,Y, or Z axes, invalid input")
+
+                    [x1,y1,z1] = np.dot(matrix,[x,y,z]) # new coordinates after rotating
+
+                    i1 = x1 + 1 # translate back to
+                    j1 = y1 + 1 # out original frame
                     k1 = z1 + 1
 
-                    # tmp_cube[i1,j1,k1] = self.cube[i,j,k]
-                    
-                    tmp = self.cube[i,j,k]
-                    tmp_cube[i1,j1,k1] = [tmp[1],-tmp[0],tmp[2]]
-                    
+                    tmp = self.cube[i,j,k] # rotate the cubies, but keep their original orientation
+                    tmp_cube[i1,j1,k1] = np.dot(matrix,tmp) # now rotate their orientation
 
         self.cube = tmp_cube
-                    # print([x,y,z])
-                    # print([x1,y1,z1])
-                    # print()
-
-                    # print([i,j,k])
-                    # print([i1,j1,k1])
-                    # print()
 
 
-    # def rotate_cube(self,clockwise,axis):
-
-    #     position_before=[] # we will populate these lists with the
-    #     position_after=[]  # each position before and after the rotation
-
-    #     for i in range(0,3):
-    #         for j in range(0,3):
-    #             for k in range(0,3):
-
-    #                 x = i - 1 # centre the cube at 0,0,0
-    #                 y = j - 1
-    #                 z = k - 1
-
-    #                 # position_before.append([x,y,z])
-
-    #                 x_clockwise = [ [1,0,0], [0,0,1], [0,-1,0] ]
-    #                 x_anticlockwise = [ [1,0,0], [0,0,-1], [0,1,0] ]
-    #                 y_clockwise = [ [0,0,-1], [0,1,0], [1,0,0] ]
-    #                 y_anticlockwise = [ [0,0,1], [0,1,0], [-1,0,0] ]
-    #                 z_clockwise = [ [0,1,0], [-1,0,0], [0,0,1] ]
-    #                 z_anticlockwise = [ [0,-1,0], [1,0,0], [0,0,1] ]
-
-    #                 rotation_matrices =[x_clockwise,x_anticlockwise,y_clockwise,y_anticlockwise,z_clockwise,z_anticlockwise]
-
-    #                 if axis == "X":
-    #                     if clockwise == 1:
-    #                         matrix = rotation_matrices[0]
-    #                     else:
-    #                         matrix = rotation_matrices[1]
-    #                 elif axis == "Y":
-    #                     if clockwise == 1:
-    #                         matrix = rotation_matrices[2]
-    #                     else:
-    #                         matrix = rotation_matrices[3]
-    #                 elif axis == "Z":
-    #                     if clockwise == 1:
-    #                         matrix = rotation_matrices[4]
-    #                     else:
-    #                         matrix = rotation_matrices[5]
-    #                 else:
-    #                     print("can only rotate about X,Y, or Z axes, invalid input")
-
-
-    #                 position_after = np.dot(matrix,[x,y,z])
-    #                 i1 = position_after[0] + 1
-    #                 j1 = position_after[1] + 1
-    #                 k1 = position_after[2] + 1
-
-    #                 self.cube[i,j,k] = self.cube[i1,j1,k1] # this is the rotation
-
-
-
-        #             position_after.append(list(np.dot(matrix,[x,y,z])+[1,1,1]))
-
-        # # print(self.cube[position_before[1]])
-        # # print(self.cube[position_before[0]])
-        # print(self.cube position_after[0])
 
 c = Cube()
 print(c.cube[2,0,0])
 print("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~")
-c.rotate_z()
+c.rotate_cube(1,"Z")
+# c.rotate_z()
 # print()
 print(c.cube[2,0,0])
 # print(c.cube[0,0])
