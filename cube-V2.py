dd5c541cb59ff7b89c657b342378df2dab59aa75
Dan Quill <dquill5@gmail.com>
2016-12-13 21:41:41 +0000

    Added a method to manually print the cube - I labelled all faces like X1 X2 X3 X8 X0 X4 X7 X6 X5 where X is LUFDRB. then I selected each cube manually

--- cube-V2.py
+++ cube-V2.py
@@ -50,12 +50,12 @@ class Cube:
 # the face facing us (X=2) is F
 # F:X=2, L:Y=0, R:Y=2, B:X=0, U:Z=2, D:Z=0
 # multiply the negative faces by -1 to only print out positive numbers
-        self.front = self.cube[2,:,:][:,:,0] # the first indexer gets us the X=2 plane, and the second indexer gets us all the cx from that
-        self.back = self.cube[0,:,:][:,:,0]*-1 # same, but for X=0 plane
-        self.left = self.cube[:,0,:][:,:,1]*-1 # all cy in Y=0 plane
-        self.right = self.cube[:,2,:][:,:,1] # smae for Y=2
-        self.upper = self.cube[:,:,2][:,:,2] # cz for Z=2
-        self.down = self.cube[:,:,0][:,:,2]*-1 # cz for Z=0
+        self.front = self.cube[2,:,:][:,:,0][::-1] # the first indexer gets us the X=2 plane, and the second indexer gets us all the cx from that
+        self.back = self.cube[0,:,:][:,:,0][::-1]*-1 # same, but for X=0 plane
+        self.left = self.cube[:,0,:][:,:,1][::-1]*-1 # all cy in Y=0 plane
+        self.right = self.cube[:,2,:][:,:,1][::-1] # smae for Y=2
+        self.upper = self.cube[:,:,2][:,:,2][::-1] # cz for Z=2
+        self.down = self.cube[:,:,0][:,:,2][::-1]*-1 # cz for Z=0
         print("         ",self.upper[0])
         print("         ",self.upper[1])
         print("         ",self.upper[2])
@@ -67,6 +67,75 @@ class Cube:
         print("         ",self.down[0])
         print("         ",self.down[1])
         print("         ",self.down[2])
+
+    def print_cube2(self):
+# redoing the printing stage "manually" like
+#[["U1","U2","U3"],["U8","U0","U4"],["U7","U6","U5"]]
+#[["L1","L2","L3"],["L8","L0","L4"],["L7","L6","L5"]]
+#[["F1","F2","F3"],["F8","F0","F4"],["F7","F6","F5"]] i.e. each face numbers go
+#[["R1","R2","R3"],["R8","R0","R4"],["R7","R6","R5"]] 1 2 3
+#[["B1","B2","B3"],["B8","B0","B4"],["B7","B6","B5"]] 8 0 4
+#[["D1","D2","D3"],["D8","D0","D4"],["D7","D6","D5"]] 7 6 5
+        # f0 = self.cube[1,0,1][1]
+        f1 = -self.cube[0,0,2][1]
+        f2 = -self.cube[1,0,2][1]
+        f3 = -self.cube[2,0,2][1]
+        f4 = -self.cube[2,0,1][1]
+        f5 = -self.cube[2,0,0][1]
+        f6 = -self.cube[1,0,0][1]
+        f7 = -self.cube[0,0,0][1]
+        f8 = -self.cube[0,0,1][1]
+        f0 = -self.cube[1,0,1][1]
+
+        L1 = -self.cube[0,2,2][0]
+        L2 = -self.cube[0,1,2][0]
+        L3 = -self.cube[0,0,2][0]
+        L4 = -self.cube[0,0,1][0]
+        L5 = -self.cube[0,0,0][0]
+        L6 = -self.cube[0,1,0][0]
+        L7 = -self.cube[0,2,0][0]
+        L8 = -self.cube[0,2,1][0]
+        L0 = -self.cube[0,1,1][0]
+
+        R1 = self.cube[2,0,2][0]
+        R2 = self.cube[2,1,2][0]
+        R3 = self.cube[2,2,2][0]
+        R4 = self.cube[2,2,1][0]
+        R5 = self.cube[2,2,0][0]
+        R6 = self.cube[2,1,0][0]
+        R7 = self.cube[2,0,0][0]
+        R8 = self.cube[2,0,1][0]
+        R0 = self.cube[2,1,1][0]
+
+        B1 = self.cube[2,2,2][1]
+        B2 = self.cube[1,2,2][1]
+        B3 = self.cube[0,2,2][1]
+        B4 = self.cube[0,2,1][1]
+        B5 = self.cube[0,2,0][1]
+        B6 = self.cube[1,2,0][1]
+        B7 = self.cube[2,2,0][1]
+        B8 = self.cube[2,2,1][1]
+        B0 = self.cube[1,2,1][1]
+
+        U1 = self.cube[0,2,2][2]
+        U2 = self.cube[1,2,2][2]
+        U3 = self.cube[2,2,2][2]
+        U4 = self.cube[2,1,2][2]
+        U5 = self.cube[2,0,2][2]
+        U6 = self.cube[1,0,2][2]
+        U7 = self.cube[0,0,2][2]
+        U8 = self.cube[0,1,2][2]
+        U0 = self.cube[1,1,2][2]
+
+        D1 = -self.cube[0,0,0][2]
+        D2 = -self.cube[1,0,0][2]
+        D3 = -self.cube[2,0,0][2]
+        D4 = -self.cube[2,1,0][2]
+        D5 = -self.cube[2,2,0][2]
+        D6 = -self.cube[1,2,0][2]
+        D7 = -self.cube[0,2,0][2]
+        D8 = -self.cube[0,1,0][2]
+        D0 = -self.cube[1,1,0][2]
 
 # Next, we need to implement rotations of the cube, where we don't
 # perform any twist, just a rotation in space
@@ -135,14 +204,7 @@ class Cube:
         self.cube[2,:,:][:,0,:] = self.cube[2,:,:][2,:,:]
         self.cube[2,:,:][2,:,:] = self.cube[2,:,:][:,2,:][::-1]
         self.cube[2,:,:][:,2,:] = tmp
-        # print("~~~~~~~~~~~~~~~~~~~~~~~~~~~~")
-        # print(self.cube[2,:,:])
 
-        # print(self.cube[2,:,:])
-        # self.cube[2,:,:][:,0,:] = self.cube[2,:,:][2,:,:][::-1]
-        # self.cube[2,:,:][2,:,:] = self.cube[2,:,:][:,2,:]
-        # self.cube[2,:,:][:,2,:] = tmp
-        # print(self.cube[2,:,:])
 # now reorient the cubies
         # tmp2 = self.cube
         # tmp2 = np.array([list(self.cube[2,:,:][x]) for x in range(0,3)])
@@ -155,12 +217,25 @@ class Cube:
         # print(self.cube)
 
 c = Cube()
-# print(c.cube[2,:,:][:,2,:])
+print(c.cube[0,0,0][2])
+print(c.cube[1,0,0][2])
+print(c.cube[2,0,0][2])
+print(c.cube[2,1,0][2])
+print(c.cube[2,2,0][2])
+print(c.cube[1,2,0][2])
+print(c.cube[0,2,0][2])
+print(c.cube[0,1,0][2])
+print(c.cube[1,1,0][2])
+
+# # c.print_cube2()
 # c.print_cube()
 # print("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~")
-c.rotate_cube(1,"Z")
-c.twist_F()
-# print(c.cube[2,:,:])
+# # # c.rotate_cube(1,"Z")
+# # c.twist_F()
+# c.twist_F()
+
+# print(c.cube)
+# # print(c.cube[2,:,:])
 
 # c.print_cube()
 # print(c.cube[2,:,:])
